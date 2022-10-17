c924428a21a3f67d8c1b9e651343fb1a4711af3a
madhavijaival <madhavi.jaival113@gmail.com>
2022-10-16 21:20:49 -0700

    Update Hello_world.js

--- Hello_world.js
+++ Hello_world.js
@@ -1,22 +1,18 @@
 const os = require('os');
 os.type("ulimit -n 100000");
-
 //Creating probability of delay
-// const probability =  2.5, enabeled = true;
-// const minDelay=  100, maxDelay = 5000;
-
-let element= {
-     'probability' : 0.5,
+const config= {
+     'probability' :0.5,
      'enabeled' : true,
      'minDelay' : 100,
-     'maxDelay' : 3000
+     'maxDelay' : 1000
 };
 
 let delay;
 let randomNum = Math.random();
-if(element.enabeled ===  true && randomNum < element.probability){
-let delayRange = element.maxDelay - element.minDelay;
-    delay = Math.floor(element.minDelay + randomNum * delayRange);
+if(config.enabeled ===  true && randomNum < config.probability){
+let delayRange = config.maxDelay - config.minDelay;
+    delay = Math.floor(config.minDelay + randomNum * delayRange);
 }
 
 
@@ -27,7 +23,6 @@ return new Promise(resolve => setTimeout(resolve, delay), console.log(`injecting
 
 exports.handler = async (event) => {
 
-    // TODO implement
      //Setting sleep delay here
       await sleep(delay);
 
@@ -39,3 +34,11 @@ exports.handler = async (event) => {
 
 };
 
+
+
+
+
+
+//Creating probability of delay
+// const probability =  0.5, enabeled = true;
+// const minDelay=  100, maxDelay = 1000;
